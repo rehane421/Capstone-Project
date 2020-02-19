b4194d268a5dac681fb77f63f417f2e6cdbf6bbd
rehane421 <rehane421@gmail.com>
2020-02-19 17:08:53 +0530

    removed virtual scrolling

--- yoyogift-ui/src/modules/gifts/components/GiftsList.js
+++ yoyogift-ui/src/modules/gifts/components/GiftsList.js
@@ -188,29 +188,6 @@ class GiftsList extends React.Component {
     page: 0,
     rowsPerPage: 10
   };
-  rowRenderer = ({
-    key, // Unique key within array of rows
-    index, // Index of row within collection
-    isScrolling, // The List is currently being scrolled
-    isVisible, // This row is visible within the List (eg it is not an overscanned row)
-    style // Style object to be applied to row (to position it)
-  }) => {
-    let { giftCardsFiltered, searchedCard, userDetails } = this.props;
-    return (
-      <div key={key} style={style}>
-        {searchedCard
-          // .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-          .map(giftCard => {
-            return (
-              <Grid item xs={12} sm={6} md={3} lg={3}>
-                <GiftCard giftCard={giftCard} userEmail={userDetails.email} />
-                {/* {(userDetails.email === "lathak95@gmail.com" || this.props.userDetails.email === "yoyogiftg2@gmail.com")? <Button variant="contained" color="primary" onClick={()=> this.props.handleUpdateClick(giftCard.id)}>UPDATE</Button> : null} */}
-              </Grid>
-            );
-          })}
-      </div>
-    );
-  };
 
   handleChangePage = (event, page) => {
     this.setState({ page });
@@ -222,31 +199,37 @@ class GiftsList extends React.Component {
 
   render() {
     let { classes, giftCardsFiltered, userDetails, searchedCard } = this.props;
-    // const { rowsPerPage, page } = this.state;
-    // const emptyRows =
-    //   rowsPerPage -
-    //   Math.min(rowsPerPage, searchedCard.length - page * rowsPerPage);
+    const { rowsPerPage, page } = this.state;
+    const emptyRows =
+      rowsPerPage -
+      Math.min(rowsPerPage, searchedCard.length - page * rowsPerPage);
     return (
       <Paper className={classes.root}>
         <div className={classes.tableWrapper}>
           <Table className={classes.table}>
             <TableBody>
               <Grid container spacing={16}>
-                <List
-                  width={1000}
-                  height={600}
-                  rowHeight={315}
-                  rowRenderer={this.rowRenderer}
-                  rowCount={giftCardsFiltered.length}
-                />
+                {searchedCard
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map(giftCard => {
+                    return (
+                      <Grid item xs={12} sm={6} md={3} lg={3}>
+                        <GiftCard
+                          giftCard={giftCard}
+                          userEmail={userDetails.email}
+                        />
+                        {/* {(userDetails.email === "lathak95@gmail.com" || this.props.userDetails.email === "yoyogiftg2@gmail.com")? <Button variant="contained" color="primary" onClick={()=> this.props.handleUpdateClick(giftCard.id)}>UPDATE</Button> : null} */}
+                      </Grid>
+                    );
+                  })}
               </Grid>
-              {/* {emptyRows > 0 && (
+              {emptyRows > 0 && (
                 <TableRow style={{ height: 48 * emptyRows }}>
                   <TableCell colSpan={6} />
                 </TableRow>
-              )} */}
+              )}
             </TableBody>
-            {/* <TableFooter>
+            <TableFooter>
               <TableRow>
                 <TablePagination
                   rowsPerPageOptions={[10, 20]}
@@ -263,7 +246,7 @@ class GiftsList extends React.Component {
                   props
                 />
               </TableRow>
-            </TableFooter> */}
+            </TableFooter>
           </Table>
         </div>
       </Paper>
